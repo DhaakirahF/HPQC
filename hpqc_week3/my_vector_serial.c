8a4194a9062f7322807b31ee82b82c92f359ff9b
DhaakirahF <dhaakirah.fasetire2@mail.dcu.ie>
2026-04-19 19:47:16 +0100

    Add week 3

--- hpqc_week3/my_vector_serial.c
+++ hpqc_week3/my_vector_serial.c
@@ -5,41 +5,59 @@
 int check_args(int argc, char **argv);
 void initialise_vector(int vector[], int size, int initial);
 void fill_vector(int vector[], int size);
-void print_vector(int vector[], int size);
 int sum_vector(int vector[], int size);
+double to_second_float(struct timespec in_time);
+struct timespec calculate_runtime(struct timespec start_time, struct timespec end_time);
 
 int main(int argc, char **argv)
 {
+    // declare and initialise the numerical argument variable
     int num_arg = check_args(argc, argv);
 
+    // creates a vector variable dynamically
     int *my_vector = malloc(num_arg * sizeof(int));
     if (my_vector == NULL)
     {
-        fprintf(stderr, "Memory allocation failed\n");
+        fprintf(stderr, "Memory allocation failed.\n");
         exit(-1);
     }
 
+    // initialises every element to zero
     initialise_vector(my_vector, num_arg, 0);
 
-    clock_t start_time, end_time;
-    double run_time;
+    // timing variables
+    struct timespec start_time, end_time, time_diff;
+    double runtime = 0.0;
 
-    start_time = clock();
+    // gets the time before the core work
+    timespec_get(&start_time, TIME_UTC);
 
+    // fills the vector with meaningful values
     fill_vector(my_vector, num_arg);
+
+    // sums the vector
     int my_sum = sum_vector(my_vector, num_arg);
 
-    end_time = clock();
+    // gets the time after the core work
+    timespec_get(&end_time, TIME_UTC);
 
-    run_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    // calculates the runtime
+    time_diff = calculate_runtime(start_time, end_time);
+    runtime = to_second_float(time_diff);
 
+    // prints the sum
     printf("Sum: %d\n", my_sum);
-    printf("Internal runtime: %f seconds\n", run_time);
 
+    // prints the internal runtime
+    printf("Runtime for core operations: %lf seconds.\n", runtime);
+
+    // free allocated memory
     free(my_vector);
+
     return 0;
 }
 
+// defines a function to sum a vector of ints into another int
 int sum_vector(int vector[], int size)
 {
     int sum = 0;
@@ -52,6 +70,7 @@ int sum_vector(int vector[], int size)
     return sum;
 }
 
+// defines a function to initialise all values in a vector to a given initial value
 void initialise_vector(int vector[], int size, int initial)
 {
     for (int i = 0; i < size; i++)
@@ -60,6 +79,7 @@ void initialise_vector(int vector[], int size, int initial)
     }
 }
 
+// fills the vector with non-trivial values
 void fill_vector(int vector[], int size)
 {
     for (int i = 0; i < size; i++)
@@ -68,14 +88,7 @@ void fill_vector(int vector[], int size)
     }
 }
 
-void print_vector(int vector[], int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d\n", vector[i]);
-    }
-}
-
+// defines a function that checks your arguments
 int check_args(int argc, char **argv)
 {
     int num_arg = 0;
@@ -92,4 +105,39 @@ int check_args(int argc, char **argv)
     }
 
     return num_arg;
+}
+
+double to_second_float(struct timespec in_time)
+{
+    double out_time = 0.0;
+    long int seconds, nanoseconds;
+    seconds = nanoseconds = 0;
+
+    seconds = in_time.tv_sec;
+    nanoseconds = in_time.tv_nsec;
+
+    out_time = seconds + nanoseconds / 1e9;
+
+    return out_time;
+}
+
+struct timespec calculate_runtime(struct timespec start_time, struct timespec end_time)
+{
+    struct timespec time_diff;
+    long int seconds, nanoseconds;
+    seconds = nanoseconds = 0;
+
+    seconds = end_time.tv_sec - start_time.tv_sec;
+    nanoseconds = end_time.tv_nsec - start_time.tv_nsec;
+
+    if (nanoseconds < 0)
+    {
+        seconds = seconds - 1;
+        nanoseconds = ((long int)1e9) + nanoseconds;
+    }
+
+    time_diff.tv_sec = seconds;
+    time_diff.tv_nsec = nanoseconds;
+
+    return time_diff;
 }
